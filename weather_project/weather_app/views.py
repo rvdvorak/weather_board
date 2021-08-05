e8b8b6caaad921c025a8dd0f37faecdcde7d6b8f
R. V. Dvorak <rdvo86@gmail.com>
2021-08-05 21:43:21 +0200

    Show current temperature for fixed location

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+import requests
+
 
 def home(request):
     return render(request, 'weather_app/home.html')
 
 def current(request):
-    return render(request, 'weather_app/current.html')
+    response = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=49.2075&lon=16.6861&exclude=minutely,hourly,daily,alerts&appid=6fe37effcfa866ecec5fd235699a402d&units=metric')
+    weather = response.json()
+    return render(request, 'weather_app/current.html', {'weather': weather})
 
 def forecast(request):
     return render(request, 'weather_app/forecast.html')
