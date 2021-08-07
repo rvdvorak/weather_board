295a98d41e05e1d12332e3001d7e79497e1e7b20
R. V. Dvorak <rdvo86@gmail.com>
2021-08-07 06:24:03 +0200

    Current weather conditions for fixed location

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests
+import datetime
 
 
 def home(request):
@@ -8,7 +9,14 @@ def home(request):
 def current(request):
     response = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=49.2075&lon=16.6861&exclude=minutely,hourly,daily,alerts&appid=6fe37effcfa866ecec5fd235699a402d&units=metric')
     weather = response.json()
-    return render(request, 'weather_app/current.html', {'weather': weather})
+    ts = int(weather['current']['dt'])
+    dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    sunrise_ts = int(weather['current']['sunrise'])
+    sunrise = datetime.datetime.fromtimestamp(sunrise_ts).strftime('%Y-%m-%d %H:%M:%S')
+    sunset_ts = int(weather['current']['sunset'])
+    sunset = datetime.datetime.fromtimestamp(sunset_ts).strftime('%Y-%m-%d %H:%M:%S')
+
+    return render(request, 'weather_app/current.html', {'weather': weather,'dt': dt, 'sunrise': sunrise, 'sunset': sunset})
 
 def forecast(request):
     return render(request, 'weather_app/forecast.html')
