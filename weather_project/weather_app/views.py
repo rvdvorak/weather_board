9e0beaf11efbb43b5047628cf32a179f9707b072
R. V. Dvorak <rdvo86@gmail.com>
2021-08-22 04:54:36 +0200

    Improve responsivity. Add HTTP status code check.

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -24,69 +24,80 @@ def weather_dashboard(request):
                 weather['minutely'][minute]['dt'])
         return weather
 
-    if selected_location:
-        lat = selected_location['geometry']['coordinates'][1]
-        lon = selected_location['geometry']['coordinates'][0]
-        weather_query = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=6fe37effcfa866ecec5fd235699a402d&units=metric'
-        weather = requests.get(weather_query)
-        weather = weather.json()
-        weather = convert_timestamps_to_datetime(weather)
-        chart_data = {
-            'minutely': {
-                'dt': [],
-                'precipitation': [],
-            }
-        }
-
-        for minute in weather['minutely']:
-            chart_data['minutely']['dt'].append(minute['dt'].strftime("%H:%M"))
-            chart_data['minutely']['precipitation'].append(
-                minute['precipitation'])
-
-        headline = 'Weather Dashboard'
-        return render(request, 'weather_app/weather_dashboard.html',
-                      {'selected_location': selected_location, 'weather': weather, 'headline': headline, 'chart_data': chart_data})
-    else:
-        headline = 'Weather Dashboard'
+    if not selected_location:
         message = {
-            'style': 'success',
-            'header': '',
-            'content': 'Please search location.',
+            'style': 'lightblue',
+            'header': 'Hint',
+            'content': 'Search the location to show the weather.',
         }
-        return render(request, 'weather_app/weather_dashboard.html',
-                      {'message': message})
+        return render(request, 'weather_app/weather_dashboard.html', {'message': message})
+
+    lat = selected_location['geometry']['coordinates'][1]
+    lon = selected_location['geometry']['coordinates'][0]
+    weather_query = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=6fe37effcfa866ecec5fd235699a402d&units=metric'
+    weather_response = requests.get(weather_query)
+
+    if not weather_response.status_code == 200:
+        message = {
+            'style': 'danger',
+            'header': 'Weather data error',
+            'content': f'Request for weather data failed. Please try again later. (Weather server status: {weather_response.status_code})'
+        }
+        return render(request, 'weather_app/weather_dashboard.html', {'message': message})
+
+    weather = weather_response.json()
+    weather = convert_timestamps_to_datetime(weather)
+
+    chart_data = {
+        'minutely': {
+            'dt': [],
+            'precipitation': [],
+        }
+    }
+    for minute in weather['minutely']:
+        chart_data['minutely']['dt'].append(minute['dt'].strftime("%H:%M"))
+        chart_data['minutely']['precipitation'].append(minute['precipitation'])
+
+    return render(request, 'weather_app/weather_dashboard.html',
+                  {'selected_location': selected_location, 'weather': weather, 'chart_data': chart_data})
 
 
 def search_results(request):
-    # TO DO: Ošetřit návratové kódy HTTP Response (found_locations)
     global found_locations
 
     found_locations = None
     search_text = request.GET.get('search_text')
-    openrouteservice_response = requests.get(
-        'https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf624830716a6e069742efa48b8fffc0f8fe71&size=50&text=' + search_text)
-    print('OpenRouteService response status code: ',
-          openrouteservice_response.status_code)
 
-    if openrouteservice_response.status_code == 200:
-        found_locations = openrouteservice_response.json()['features']
-        if found_locations:
-            headline = 'Select location'
-            return render(request, 'weather_app/search_results.html', {'found_locations': found_locations, 'headline': headline})
-        else:
-            message = {
-                'style': 'warning',
-                'header': 'No location found',
-                'content': 'You probably entered the location incorrectly. Please try again.',
-            }
-            return render(request, 'weather_app/search_results.html', {'message': message})
-    else:
+    if not search_text:
         message = {
-            'style': 'danger',
-            'header': 'Location server error',
-            'content': f'Something went wrong with the location server. Please try again later. (HTTP status code: {openrouteservice_response.status_code})',
+            'style': 'lightblue',
+            'header': 'Hint',
+            'content': 'First enter the name of the location you want to search.',
         }
         return render(request, 'weather_app/search_results.html', {'message': message})
+
+    location_query = 'https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf624830716a6e069742efa48b8fffc0f8fe71&size=50&text=' + search_text
+    location_response = requests.get(location_query)
+
+    if not location_response.status_code == 200:
+        message = {
+            'style': 'danger',
+            'header': 'Search location error',
+            'content': f'Search request failed. Please try again later. (Location server status: {openrouteservice_response.status_code})',
+        }
+        return render(request, 'weather_app/search_results.html', {'message': message})
+
+    found_locations = location_response.json()['features']
+
+    if not found_locations:
+        message = {
+            'style': 'lightblue',
+            'header': 'No location found',
+            'content': 'You probably entered the location incorrectly. Please try again.',
+        }
+        return render(request, 'weather_app/search_results.html', {'message': message})
+
+    return render(request, 'weather_app/search_results.html', {'found_locations': found_locations})
 
 
 def set_location(request):
