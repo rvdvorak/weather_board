3c82b54b5e1dfede0e2463ecd5614e9de33d9f9f
R. V. Dvorak <rdvo86@gmail.com>
2021-09-20 22:53:12 +0200

    Add Location model.

--- weather_project/weather_app/models.py
+++ weather_project/weather_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Location(models.Model):
+    label = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    favourite = models.BooleanField()
+    date_last_showed = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.label
