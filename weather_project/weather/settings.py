e7d89ac78abe5514fbf3669b0b1b04223a3af869
R. V. Dvorak <rdvo86@gmail.com>
2021-12-14 17:39:03 +0100

    import os

--- weather_project/weather/settings.py
+++ weather_project/weather/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import dj_database_url
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
